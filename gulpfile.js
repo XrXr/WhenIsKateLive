5ae0c9951d8265d16a1e37b58c333f5118cf9225
XrXr <xrsquared@gmail.com>
2015-11-08 18:58:46 -0500

    Clean up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,55 +4,47 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     minifyCss = require('gulp-minify-css');
 
-gulp.task('compress', function() {
-  return gulp.src('lib/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist'));
-});
-
-
-
 var ENTRY = 'src/js/main.js';
 var DEST = 'js/main.js';
 
 gulp.task('bundle', function() {
     return rollup.rollup({
         entry: ENTRY
-    }).then(function (bundle) {
-        return bundle.write({
-            format: 'iife',
-            dest: DEST
+        }).then(function (bundle) {
+            return bundle.write({
+                format: 'iife',
+                dest: DEST
+                });
+            });
         });
-    });
-});
 
 gulp.task('minify-css', function() {
     return gulp.src('src/styles/*.css')
-        .pipe(minifyCss())
-        .pipe(gulp.dest('./styles'));
-});
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./styles'));
+    });
 
 gulp.task('minify-js', ['bundle'], function() {
     return gulp.src('js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('js'));
-});
-
-gulp.task('default', ['minify-css', 'minify-js']);
+    .pipe(uglify())
+    .pipe(gulp.dest('js'));
+    });
 
 gulp.task('build-and-reload', ['minify-css', 'bundle'], function () {
     browserSync.reload();
-});
+    });
 
 gulp.task('serve', function(cb) {
-  browserSync({
-    server: {
-      baseDir: './'
-    },
-    notify: false,
-    ui: false,
-    ghostMode: false
-  });
+    browserSync({
+        server: {
+            baseDir: './'
+        },
+        notify: false,
+        ui: false,
+        ghostMode: false
+    });
 
-  gulp.watch(['*.html', 'styles/**/*.css', 'js/**/*.js'], {cwd: 'src'}, ['build-and-reload']);
+    gulp.watch(['*.html', 'styles/**/*.css', 'js/**/*.js'], {cwd: 'src'}, ['build-and-reload']);
 });
+
+gulp.task('default', ['minify-css', 'minify-js']);
