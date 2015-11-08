b98d6db25cdcb3abba070f2874a637d1c7ac621a
XrXr <xrsquared@gmail.com>
2015-11-08 12:24:22 -0500

    Ground work for breaking into modules

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    rollup = require('rollup'),
+    livereload = require('gulp-livereload');
+
+var ENTRY = 'src/js/main.js';
+var DEST = 'js/main.js';
+
+gulp.task('bundle', function() {
+    return rollup.rollup({
+        entry: ENTRY
+    }).then(function (bundle) {
+        return bundle.write({
+            format: 'iife',
+            dest: DEST
+        });
+    });
+});
+
+gulp.task('livereload', ['bundle'], function () {
+    livereload.reload();
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('src/**/*.js', ['livereload']);
+  return new Promise(function () {});
+});
