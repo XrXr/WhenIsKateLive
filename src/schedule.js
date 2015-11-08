d6ec319299963ebf63e58418570e15dd59c281a4
XrXr <xrsquared@gmail.com>
2015-11-08 12:51:47 -0500

    Take out symlink for schedule.json

--- src/schedule.js
+++ src/schedule.js
@@ -1,1 +1,32 @@
-../schedule.json
+export default [
+  {
+    "weekday": "Monday",
+    "time": "6:00 PM",
+    "duration": 2,
+    "canceled": false
+  },
+  {
+    "weekday": "Wednesday",
+    "time": "5:00 PM",
+    "duration": 2,
+    "canceled": false
+  },
+  {
+    "weekday": "Thursday",
+    "time": "9:00 PM",
+    "duration": 2,
+    "canceled": false
+  },
+  {
+    "weekday": "Saturday",
+    "time": "3:00 PM",
+    "duration": 2,
+    "canceled": false
+  },
+  {
+    "weekday": "Sunday",
+    "time": "4:00 PM",
+    "duration": 2,
+    "canceled": false
+  }
+];
